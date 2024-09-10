8d06a12cb46104dc5a3486cfc9f0c2828ad8425b
dimple <dimpleshakrawar@gmail.com>
2024-09-10 18:50:23 +0530

    added package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js', // Ensure PostCSS config is used
+    postcss: "./postcss.config.js",
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
